ce0b072eb280e35a6d86b49eefadaf383ba29415
jemek27 <emajure27@gmail.com>
2025-04-11 00:37:44 +0200

    feat(Ingredient): get ingredients of a Dish

--- src/main/java/com/jureczko/take/service/IngredientService.java
+++ src/main/java/com/jureczko/take/service/IngredientService.java
@@ -1,17 +1,22 @@
 package com.jureczko.take.service;
 
 import com.jureczko.take.exception.ResourceNotFoundException;
+import com.jureczko.take.model.Dish;
 import com.jureczko.take.model.Ingredient;
+import com.jureczko.take.model.Recipe;
+import com.jureczko.take.repository.DishRepository;
 import com.jureczko.take.repository.IngredientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class IngredientService {
     private final IngredientRepository ingredientRepository;
+    private final DishRepository dishRepository;
 
     public List<Ingredient> getAllIngredients() {
         return ingredientRepository.findAll();
@@ -32,5 +37,10 @@ public class IngredientService {
 
     public boolean existsById(Long id) {
         return ingredientRepository.existsById(id);
+    }
+
+    public List<Ingredient> getIngredientsByDishId(Long id) {
+        Dish dish = dishRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Dish with ID " + id + " not found"));
+        return dish.getRecipe().stream().map(Recipe::getIngredient).collect(Collectors.toList());
     }
 }
